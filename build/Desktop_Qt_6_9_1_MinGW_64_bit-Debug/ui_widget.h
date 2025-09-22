e7e6aa120ef58de115f74e554f6a320b8ab708b0
bughsu <1tom234567890@gmail.com>
2025-09-22 16:15:01 +0800

    finish project

---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/ui_widget.h
@@ -0,0 +1,83 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QSlider *horizontalSlider;
+    QSpinBox *spinBox;
+    QLCDNumber *lcdNumber;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName("Widget");
+        Widget->resize(800, 600);
+        verticalLayout = new QVBoxLayout(Widget);
+        verticalLayout->setObjectName("verticalLayout");
+        horizontalSlider = new QSlider(Widget);
+        horizontalSlider->setObjectName("horizontalSlider");
+        horizontalSlider->setMinimum(-100);
+        horizontalSlider->setMaximum(100);
+        horizontalSlider->setOrientation(Qt::Orientation::Horizontal);
+
+        verticalLayout->addWidget(horizontalSlider);
+
+        spinBox = new QSpinBox(Widget);
+        spinBox->setObjectName("spinBox");
+        QFont font;
+        font.setPointSize(36);
+        spinBox->setFont(font);
+        spinBox->setMinimum(-100);
+        spinBox->setMaximum(100);
+        spinBox->setDisplayIntegerBase(10);
+
+        verticalLayout->addWidget(spinBox);
+
+        lcdNumber = new QLCDNumber(Widget);
+        lcdNumber->setObjectName("lcdNumber");
+
+        verticalLayout->addWidget(lcdNumber);
+
+
+        retranslateUi(Widget);
+        QObject::connect(spinBox, &QSpinBox::valueChanged, horizontalSlider, &QSlider::setValue);
+        QObject::connect(horizontalSlider, &QSlider::valueChanged, spinBox, &QSpinBox::setValue);
+        QObject::connect(horizontalSlider, &QSlider::valueChanged, lcdNumber, qOverload<int>(&QLCDNumber::display));
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
